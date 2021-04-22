2e0fa79cf7daf36c209086ccf3c7f0f8882bcc91
MingyuanQin <mingyuan0715@foxmail.com>
2021-04-22 11:23:16 +0800

    rails med

--- lib/rails_med/engine.rb
+++ lib/rails_med/engine.rb
@@ -1,4 +1,12 @@
+require 'rails_com'
 module RailsMed
   class Engine < ::Rails::Engine
+
+    config.autoload_paths += Dir[
+      "#{config.root}/app/models/cause",
+      "#{config.root}/app/models/treat",
+      "#{config.root}/app/models/symptom"
+    ]
+
   end
 end
--- lib/tasks/rails_med_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :rails_med do
-#   # Task goes here
-# end
